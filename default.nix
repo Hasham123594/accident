aaf026bd5eb858b11d006a6d7d9a1f6dc8109a65
Muhammad Hasham <0240979407@uni.lu>
2026-01-06 16:00:14 +0100

    Fix runtime libraries for numpy/pandas in Nix shell

--- default.nix
+++ default.nix
@@ -5,6 +5,13 @@ pkgs.mkShell {
     git
     python312
     uv
+
+    # runtime libs commonly needed by numpy/pandas wheels
+    zlib
     stdenv.cc.cc.lib
   ];
+
+  shellHook = ''
+    export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath (with pkgs; [ zlib stdenv.cc.cc.lib ])}:$LD_LIBRARY_PATH"
+  '';
 }
